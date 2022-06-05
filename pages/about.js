34ec26806f5dc9f23030cf6dcc5ce1c9d3576170
Matias Azcui <matias.azcui@gmail.com>
2022-06-05 10:14:51 -0300

    Cambio apellido

--- pages/about.js
+++ pages/about.js
@@ -13,7 +13,7 @@ export default function IndexPage() {
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Sobre Ezequiel
+            Ezequiel Nuske
           </h1>
           <p className="mb-8 leading-relaxed">
             Chillwave portland ugh, knausgaard fam polaroid iPhone. Man braid
